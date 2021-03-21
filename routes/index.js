94b238123f89c7a20b5edd176ac1c3638c73b67a
Yelena Strizhakova <e.strizhakova@gmail.com>
2021-03-21 20:05:24 +0500

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const usersRouter = require('./users.js');
 const NotFoundError = require('../errors/not-found-err');
-const { validateCreateUser, validateLogin } = require('../middlewares/validations');
 const errorMessages = require('../configs/error-messages');
 
-// router.post('/signup', validateCreateUser, createUser);
 router.use('/users', usersRouter);
 
 router.all('*', (req, res, next) => {
